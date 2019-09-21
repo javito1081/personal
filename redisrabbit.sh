c8baa1bbacd34c2bc074877ed50e8d9c6bf93c7b
Javier Duran <javito@gmail.com>
2019-09-21 18:33:15 -0500

    Update redisrabbit.sh

--- redisrabbit.sh
+++ redisrabbit.sh
@@ -11,12 +11,12 @@ echo "#############################################"
 echo "###   Install Redis Server and Rabbitmq   ###"
 echo "#############################################"
 echo Checking Redis
-sudo apt list | grep redis-server/bionic
+apt list | grep redis-server/bionic
 if [ $? -ne 0 ]; then
      apt install redis-server -y
 fi
 echo Checking Rabbit
-sudo apt list | grep rabbitmq-server/bionic
+apt list | grep rabbitmq-server/bionic
 if [ $? -ne 0 ]; then
      apt install rabbitmq-server -y
 fi
